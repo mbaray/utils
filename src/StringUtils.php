53cf4be78ed5aa11863ad2de75faf9df5181470e
Maria <baraymasha@gmail.com>
2022-03-30 17:29:21 +0300

    Modified files

--- src/StringUtils.php
+++ src/StringUtils.php
@@ -7,6 +7,7 @@ function capitalize($text)
     if ($text === '') {
         return '';
     }
+
     $firstSymbol = mb_strtoupper($text[0]);
     $restSubstring = mb_substr($text, 1);
     return "{$firstSymbol}{$restSubstring}";
--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,13 +1,20 @@
 <?php
 
-require("src/StringUtils.php");
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/StringUtils.php';
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize(''), '');
+Assert::eq(capitalize('hello'), 'Hello');
 
-echo 'Все тесты пройдены!';
+// if (StringUtils\capitalize('hello') !== 'Hello') {
+//     throw new \Exception('Функция работает неверно!');
+// }
+
+// if (StringUtils\capitalize('') !== '') {
+//     throw new \Exception('Функция работает неверно!');
+// }
+
+// echo 'Все тесты пройдены!';
